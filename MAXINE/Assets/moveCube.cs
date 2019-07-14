0a5a68ae978bd569b4916dad86f4f0e6594b7048
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-07-13 23:35:21 -0400

    Might as well check this in.

---
+++ MAXINE/Assets/moveCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCube : MonoBehaviour
+{
+        Rigidbody _rig;
+        public float _speed = 15f;
+        // Start is called before the first frame update
+        void Start()
+        {
+                _rig = GetComponent<Rigidbody>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+                _rig.velocity = new Vector3(0, 0, _speed);
+
+        }
+}
